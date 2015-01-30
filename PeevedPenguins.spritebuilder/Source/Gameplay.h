095b19dcb3b81454b7202b1827e380c9a2871cfc
River <river@river.local>
2015-01-29 18:17:11 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by River on 1/29/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end

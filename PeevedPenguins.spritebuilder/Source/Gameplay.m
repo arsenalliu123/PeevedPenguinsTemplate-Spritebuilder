3b16078a6989019ffc9bec080cac1ee22f0eec20
River <river@river.local>
2015-01-30 23:03:22 -0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -13,6 +13,7 @@
     CCNode *_catapultArm;
     CCNode *_levelNode;
     CCNode *_contentNode;
+    CCNode *_pullbackNode;
 }
 // is called when CCB file has completed loading
 - (void)didLoadFromCCB {
@@ -21,6 +22,7 @@
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild:level];
     _physicsNode.debugDraw = true;
+    _pullbackNode.physicsBody.collisionMask = @[];1
 }
 
 // called on every touch in this scene
